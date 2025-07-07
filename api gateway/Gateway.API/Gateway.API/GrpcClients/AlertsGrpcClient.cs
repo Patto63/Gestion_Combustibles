10010ad4fb6c7080d681968657c88ab583e4e26f
Patricio Salazar <104953438+Patto63@users.noreply.github.com>
2025-07-06 20:14:47 -0500

    Refine AlertsGrpcClient namespace usage

--- api gateway/Gateway.API/Gateway.API/GrpcClients/AlertsGrpcClient.cs
+++ api gateway/Gateway.API/Gateway.API/GrpcClients/AlertsGrpcClient.cs
@@ -1,14 +1,15 @@
 using Grpc.Net.Client;
 using Gateway.API.Models;
 using Google.Protobuf.WellKnownTypes;
-using AlertsService;
+using AlertsGrpc = AlertsService;
+using AlertsGrpcService = AlertsService.AlertsService;
 using GrpcAlertDto = AlertsService.AlertaDto;
 
 namespace Gateway.API.GrpcClients;
 
 public class AlertsGrpcClient
 {
-    private readonly AlertsService.AlertsServiceClient _client;
+    private readonly AlertsGrpcService.AlertsServiceClient _client;
 
     public AlertsGrpcClient(IConfiguration configuration)
     {
@@ -17,7 +18,7 @@ public class AlertsGrpcClient
             throw new InvalidOperationException("No se configuró la URL del microservicio de alertas.");
 
         var channel = GrpcChannel.ForAddress(url);
-        _client = new AlertsService.AlertsServiceClient(channel);
+        _client = new AlertsGrpcService.AlertsServiceClient(channel);
     }
 
     public async Task<IEnumerable<AlertaDto>> GetAllAsync()
@@ -43,19 +44,19 @@ public class AlertsGrpcClient
 
     public async Task<IEnumerable<AlertaDto>> GetByVehiculoAsync(string codigo)
     {
-        var response = await _client.AlertasPorVehiculoAsync(new CodigoVehiculoRequest { CodigoVehiculo = codigo });
+        var response = await _client.AlertasPorVehiculoAsync(new AlertsGrpc.CodigoVehiculoRequest { CodigoVehiculo = codigo });
         return response.Alertas.Select(MapDto);
     }
 
     public async Task<IEnumerable<AlertaDto>> GetByConductorAsync(string codigo)
     {
-        var response = await _client.AlertasPorConductorAsync(new CodigoConductorRequest { CodigoConductor = codigo });
+        var response = await _client.AlertasPorConductorAsync(new AlertsGrpc.CodigoConductorRequest { CodigoConductor = codigo });
         return response.Alertas.Select(MapDto);
     }
 
     public async Task<IEnumerable<AlertaDto>> GetByRutaAsync(string codigo)
     {
-        var response = await _client.AlertasPorRutaAsync(new CodigoRutaRequest { CodigoRuta = codigo });
+        var response = await _client.AlertasPorRutaAsync(new AlertsGrpc.CodigoRutaRequest { CodigoRuta = codigo });
         return response.Alertas.Select(MapDto);
     }
 
@@ -63,7 +64,7 @@ public class AlertsGrpcClient
     {
         try
         {
-            var a = await _client.ObtenerAlertaAsync(new AlertaIdRequest { AlertaId = id });
+            var a = await _client.ObtenerAlertaAsync(new AlertsGrpc.AlertaIdRequest { AlertaId = id });
             return MapDto(a);
         }
         catch (Grpc.Core.RpcException ex) when (ex.StatusCode == Grpc.Core.StatusCode.NotFound)
@@ -74,7 +75,7 @@ public class AlertsGrpcClient
 
     public async Task<AlertaDto> CreateAsync(AlertaCreateRequestModel request)
     {
-        var grpcRequest = new AlertsService.AlertaCreateRequest
+        var grpcRequest = new AlertsGrpc.AlertaCreateRequest
         {
             CodigoVehiculo = request.CodigoVehiculo,
             CodigoConductor = request.CodigoConductor,
@@ -92,7 +93,7 @@ public class AlertsGrpcClient
 
     public async Task<AlertaDto> UpdateAsync(int id, AlertaUpdateRequestModel request)
     {
-        var grpcRequest = new AlertsService.AlertaUpdateRequest { AlertaId = id };
+        var grpcRequest = new AlertsGrpc.AlertaUpdateRequest { AlertaId = id };
         if (request.CodigoVehiculo != null) grpcRequest.CodigoVehiculo = request.CodigoVehiculo;
         if (request.CodigoConductor != null) grpcRequest.CodigoConductor = request.CodigoConductor;
         if (request.CodigoRuta != null) grpcRequest.CodigoRuta = request.CodigoRuta;
@@ -112,7 +113,7 @@ public class AlertsGrpcClient
     {
         try
         {
-            await _client.EliminarAlertaAsync(new AlertaIdRequest { AlertaId = id });
+            await _client.EliminarAlertaAsync(new AlertsGrpc.AlertaIdRequest { AlertaId = id });
             return true;
         }
         catch (Grpc.Core.RpcException ex) when (ex.StatusCode == Grpc.Core.StatusCode.NotFound)
